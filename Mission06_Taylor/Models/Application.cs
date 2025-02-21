258c1ec5e4e15bac655755baafd41976614bfaa4
Jaden Taylor <jtjadent@gmail.com>
2025-02-20 17:22:27 -0700

    Quick additions

--- Mission06_Taylor/Models/Application.cs
+++ Mission06_Taylor/Models/Application.cs
@@ -12,13 +12,17 @@ namespace Mission06_Taylor.Models
         [ForeignKey("CategoryId")]
         public int? CategoryId { get; set; }
         public Category? Category { get; set; }
+        [Required]
         public string Title { get; set; }
         [Range(1000, 2027)]
+        [Required]
         public int Year { get; set; }
         public string? Director { get; set; }
         public string? Rating { get; set; }
+        [Required]
         public bool Edited { get; set; }
         public string? LentTo { get; set; }
+        [Required]
         public bool CopiedToPlex { get; set; }
         public string? Notes { get; set; }
     }
